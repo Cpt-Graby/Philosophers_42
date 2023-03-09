94f9b7186fbeb2e35019f32499cc39f051378361
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-03-09 21:22:17 +0100

    Starting to play with the mutex

--- philo/srcs/philo_routine.c
+++ philo/srcs/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 18:07:13 by agonelle          #+#    #+#             */
-/*   Updated: 2023/03/09 19:00:29 by agonelle         ###   ########.fr       */
+/*   Updated: 2023/03/09 19:38:17 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 void	*routine(void *arg)
 {
 	t_philo	*philo;
+	int status;
 
 	philo = (t_philo *)arg;
 	printf("Hello from philo %d\n", philo->id);
 	printf("I have to eat with the forks:\n");
 	printf("fork1: %p\n", philo->self_fork);
 	printf("fork2: %p\n", philo->left_fork);
+	pthread_mutex_lock(&philo->self_fork->lock);
+	printf("lock mutex fork1: %d\n", &philo->self_fork->lock);
 	return (NULL);
 }
